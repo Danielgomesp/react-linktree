34e930b1526a7f814d235a72a64561b9d02f5223
danielgomesp <danielgomesp@gmail.com>
2020-03-15 11:33:08 -0300

    perf: add personal title and subtitle

--- src/components/LinkTree/index.js
+++ src/components/LinkTree/index.js
@@ -7,7 +7,7 @@ import Header from '../Header';
 export default function LinkTree() {
   return (
     <Container>
-      <Header picture='https://scontent.fgvr2-1.fna.fbcdn.net/v/t31.0-8/p960x960/13710496_10209793534625506_2997078028056513758_o.jpg?_nc_cat=107&_nc_sid=7aed08&_nc_ohc=CT0MJiwW7cYAX-DuiSm&_nc_ht=scontent.fgvr2-1.fna&_nc_tp=6&oh=f10204fc396130e703c690b8be97f3c2&oe=5E94D2C2' title='Your Title Goes Here' subtitle='And here is your subtitle or any additional text' />
+      <Header picture='https://scontent.fgvr2-1.fna.fbcdn.net/v/t31.0-8/p960x960/13710496_10209793534625506_2997078028056513758_o.jpg?_nc_cat=107&_nc_sid=7aed08&_nc_ohc=CT0MJiwW7cYAX-DuiSm&_nc_ht=scontent.fgvr2-1.fna&_nc_tp=6&oh=f10204fc396130e703c690b8be97f3c2&oe=5E94D2C2' title='Daniel Gomes Pereira' subtitle='A Brazilian Frontend Developer' />
       <Button link='https://www.facebook.com/danielgomesp' icon='💯' name='Facebook' backgroundColor={variables.facebookColor} />
       <Button link='https://www.instagram.com/danielgomesp/' icon='💯' name='Instagram' backgroundColor={variables.instagramColor} />
       <Button link='https://www.youtube.com/channel/UCrVYzSKkbkJuCtXFIPAc1LA?view_as=subscriber' icon='😀' name='Youtube' backgroundColor={variables.youtubeColor} />
